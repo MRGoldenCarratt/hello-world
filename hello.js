56fe013314e3fc148fcd6e4f5e04fa244b27c680
MRGoldenCarratt <43500392+MRGoldenCarratt@users.noreply.github.com>
2018-12-20 15:50:15 +0000

    Delete hello.js

--- hello.js
+++
@@ -1,6 +0,0 @@
-while("a"==="a"){
-setTimeout(document.getElementById("hello").innerHTML = "hello developer", 1000);
-setTimeout(document.getElementById("hello").innerHTML = "hello craetor", 1000);
-setTimeout(document.getElementById("hello").innerHTML = "hello people", 1000);
-setTimeout(document.getElementById("hello").innerHTML = "hello world", 1000);
-}
